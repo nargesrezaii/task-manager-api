fc5d6d33e5d39ee4861281eece8da94ba213e695
nargesrezaii <nargesrezaeiiiii@gmail.com>
2026-08-13 22:38:35 +0330

    feat: add task serializer validation
    Validate task titles for empty and excessive values.
    Add validation for task status and due date.

--- apps/tasks/serializers.py
+++ apps/tasks/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.utils import timezone
 
 from apps.tasks.models import Task
 
@@ -7,20 +8,37 @@ class TaskSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(
         source = "owner.username"    
     )
+
+    def validate_title(self, value):
+        if not value.strip():
+            raise serializers.ValidationError(
+                "Title cannot be empty."    
+        )
+        
+        if len(value)>200:
+            raise serializers.ValidationError(
+                "Title cannot exceed 200 characters."
+            )
+
+        return value
     
+    def validate(self, attrs):
+        status = attrs.get("status")
+        due_date = attrs.get("due_date")
+
+        if (
+            status == "COMPLETED"
+            and due_date
+            and due_date > timezone.now()
+        ): 
+            raise serializers.ValidationError(
+                "A completed task cannot have a future due date."    
+            )
+        return attrs
+
     class Meta:
         model = Task
-        fields = (
-            "id",
-            "owner",
-            "title",
-            "description",
-            "status",
-            "priority",
-            "due_date",
-            "created_at",
-            "updated_at",
-        )
+        fields = "__all__"
         
         read_only_fields = (
             "id",
