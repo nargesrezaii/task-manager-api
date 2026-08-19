e8d01bdc702f7ccd003ede92fd60b4fb3f9bc226
nargesrezaii <nargesrezaeiiiii@gmail.com>
2026-08-19 18:05:01 +0330

    feat: add task completion action
    Add custom ViewSet action for completing tasks.

--- apps/tasks/views.py
+++ apps/tasks/views.py
@@ -1,5 +1,7 @@
-from rest_framework import viewsets, filters
+from rest_framework import serializers, viewsets, filters
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from apps.tasks.models import Task
 from apps.tasks.serializers import TaskSerializer
@@ -44,3 +46,14 @@ class TaskViewSet(viewsets.ModelViewSet):
         serializer.save(
             owner = self.request.user
         )
+
+    @action(detail=True, methods=["post"])
+    def complete(self, request, pk=None):
+        task = self.get_object()
+
+        task.status = Task.Status.COMPLETED
+        task.save(update_fields=["status"])
+        
+        serializer = self.get_serializer()
+
+        return Response(serializer.data)
