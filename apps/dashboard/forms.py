bead53f0eb32afaff4164a82269837ae01fce6e4
nargesrezaii <nargesrezaeiiiii@gmail.com>
2026-08-25 16:50:24 +0330

    fix: remove unnecessary imports

--- apps/dashboard/forms.py
+++ apps/dashboard/forms.py
@@ -1,4 +1,3 @@
-from tkinter import W
 from django import forms
 
 from apps.tasks.models import Task
--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -1,4 +1,3 @@
-import stat
 from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
