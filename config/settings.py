2135b32b08c31370e6858d844d79fe12673bb0ad
nargesrezaii <nargesrezaeiiiii@gmail.com>
2026-09-05 20:37:19 +0330

    feat: add static file serving with nginx

--- config/settings.py
+++ config/settings.py
@@ -132,11 +132,18 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = 'static/'
-
 AUTH_USER_MODEL = 'users.User'
 
 MEDIA_URL = '/media/'
+
 MEDIA_ROOT = BASE_DIR / 'media'
 
 LOGIN_URL = "login"
+
+STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",    
+]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
