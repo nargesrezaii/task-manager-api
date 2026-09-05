a4b048b101638ff4b1bfaea5db4d3e57ec08327d
nargesrezaii <nargesrezaeiiiii@gmail.com>
2026-09-05 17:22:36 +0330

    chore: make database configuration environment-driven

--- config/settings.py
+++ config/settings.py
@@ -63,8 +63,18 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.environ.get(
+            "DB_ENGINE",
+            "django.db.backends.sqlite3",
+        ),
+        'NAME': os.environ.get(
+            "DB_NAME",
+            BASE_DIR / "fb.sqlite3",
+        ),
+        "USER": os.environ.get("DB_USER", ""),
+        "PASSWORD": os.environ.get("DB_PASSWORD", ""),
+        "HOST": os.environ.get("DB_HOST", ""),
+        "PORT": os.environ.get("DB_PORT", ""),
     }
 }
 
