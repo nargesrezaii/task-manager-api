62467f8027be779a68f186840b16eeaac354f030
nargesrezaii <nargesrezaeiiiii@gmail.com>
2026-09-06 15:32:51 +0330

    style: improve dashboard and authentication changes

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,7 @@ load_dotenv(BASE_DIR / ".env")
 
 SECRET_KEY = os.environ["SECRET_KEY"]
 
-DEBUG = os.environ.get("DEBUG", "FALSE") == "TRUE"
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
 
